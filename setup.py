9c67278f5d40fd72d6fc9b180a6d9a9a22c05b78
Wessel van Lit <wessel@litdev.nl>
2020-11-25 17:49:52 +0100

    Updated requirements and setup

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
 setup(
     name='wilde',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'Click',
-        'pydub',
-        'inaSpeechSegmenter',
-    ],
+    install_requires=requirements,
     entry_points='''
         [console_scripts]
         wilde=wilde.main:find_music_in_file
     ''',
-    url='https://github.com/wvanlit/Wilde'
+    url='https://github.com/wvanlit/wilde'
 )
